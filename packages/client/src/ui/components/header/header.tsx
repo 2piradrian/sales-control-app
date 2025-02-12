41c720d372d9090e83aaa195d0449686d0a09440
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-02-12 18:57:21 -0300

    fix: header width

--- packages/client/src/ui/components/header/header.tsx
+++ packages/client/src/ui/components/header/header.tsx
@@ -4,16 +4,18 @@ import style from './style.module.css';
 
 export default function Header() {
   return (
-    <header className={`${style.container} delimiter`}>
-      <nav className={style.routesContainer}>
-        <div className={style.routes}>
-            <NavRoute name="Inicio" route="/" />
-            <NavRoute name="Productos" route="/products" />
-        </div>
-        <div className={style.user}>
-            <UserIcon name="Adrián Rodriguez" />
-        </div>
-      </nav>
+    <header className={`${style.container} container`}>
+      <div className={`${style.delimiter} delimiter`}>
+        <nav className={style.routesContainer}>
+          <div className={style.routes}>
+              <NavRoute name="Inicio" route="/" />
+              <NavRoute name="Productos" route="/products" />
+          </div>
+          <div className={style.user}>
+              <UserIcon name="Adrián Rodriguez" />
+          </div>
+        </nav>
+      </div>
     </header>
   );
 }
