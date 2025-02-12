c7e116b9b4227e5b7bee90783cf34a7877d2464e
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-02-12 16:42:52 -0300

    add: home route structure

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import RoutesManager from './ui/routes/routes-manager'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hola</h1>
+    <RoutesManager />
   </StrictMode>,
 )
