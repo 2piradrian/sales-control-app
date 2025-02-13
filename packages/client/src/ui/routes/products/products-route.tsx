5ecf4bfc131d163a94c6c56cb60a03c6a3323880
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-02-13 18:17:32 -0300

    add: create and edit screens

--- packages/client/src/ui/routes/products/products-route.tsx
+++ packages/client/src/ui/routes/products/products-route.tsx
@@ -14,13 +14,13 @@ export default function ProductsRoute() {
                         name="Registrá nuevos productos" 
                         description="Agregá nuevos productos al sistema." 
                         icon="/icons/addfolder.svg"
-                        path=""
+                        path="/products/create"
                     />
                     <FeatureButton 
                         name="Editá productos existentes" 
                         description="Modificá la información de los productos existentes en el sistema." 
                         icon="/icons/pencil.svg"
-                        path=""
+                        path="/products/edit"
                     />
                     <FeatureButton 
                         name="Registrá una compra" 
--- packages/client/src/ui/routes/routes-manager.tsx
+++ packages/client/src/ui/routes/routes-manager.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeRoute from "./home/home-route";
 import ProductsRoute from "./products/products-route";
+import ProductsCreate from "./products-create/products-create";
+import ProductsEdit from "./products-edit/products-edit";
 
 export default function RoutesManager() {
     return(
@@ -8,7 +10,11 @@ export default function RoutesManager() {
             <Routes>
                 <Route path="/" element={<HomeRoute />} />
                 <Route path="/dashboard" element={<HomeRoute />} />
+
                 <Route path="/products" element={<ProductsRoute />} />
+                <Route path="/products/create" element={<ProductsCreate />} />
+                <Route path="/products/edit" element={<ProductsEdit />} />
+                
                 <Route path="/sales" element={<HomeRoute />} />
                 <Route path="/users" element={<HomeRoute />} />
             </Routes>
