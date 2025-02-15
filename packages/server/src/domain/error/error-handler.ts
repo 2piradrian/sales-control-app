d6bb2dc4a59f4c1f6e61fd7f608c0fb7bf5e16e0
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-02-15 15:28:20 -0300

    add: implement error handling and error types for improved error management

---
+++ packages/server/src/domain/error/error-handler.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { ErrorType } from "./error-types";
+
+export class ErrorHandler extends Error {
+
+    constructor(
+        public readonly statusCode: number,
+        public readonly message: string
+    ) {
+        super(message);
+    }
+
+    static handle = (error: unknown, req: Request, res: Response ) => {
+        
+        console.error(error);
+
+        if (error instanceof ErrorHandler) {
+            return res.status(error.statusCode).json({ error: error.message });
+        }
+        return res.status(500).json({ error: ErrorType.InternalError });
+    }
+
+}
---
+++ packages/server/src/domain/error/error-types.ts
@@ -0,0 +1,12 @@
+export enum ErrorType {
+	MissingFields           = "Missing fields",
+	InvalidFields           = "Invalid fields",
+	InternalError           = "Internal error",
+	InvalidPassword         = "Invalid password",
+	Unknown                 = "Unknown error",
+	UserAlreadyExists       = "User already exists",
+	UserNotFound            = "User not found",
+	Unauthorized            = "Unauthorized",
+	Disconnected            = "Disconnected",
+	InvalidSecret           = "Invalid secret",
+}
