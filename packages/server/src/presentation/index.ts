33f33f079beb6a69226b60186f713804da52af67
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-02-16 18:36:41 -0300

    add: export product controller, routes, service, and secret middleware in index

--- packages/server/src/presentation/index.ts
+++ packages/server/src/presentation/index.ts
@@ -1,2 +1,6 @@
+export * from './app/products/controller';
+export * from './app/products/routes';
+export * from './app/products/service';
+export * from './middleware/secret';
 export * from './server/router';
 export * from './server/server';
