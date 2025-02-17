c60534701e0e4c785de14e941820e5361c6a9403
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-02-16 21:16:34 -0300

    refactor: restructure model initialization and setup for Sequelize in PostgresqlDatabase

--- packages/server/src/data/postgresql/models/status.ts
+++ packages/server/src/data/postgresql/models/status.ts
@@ -1,34 +1,31 @@
-import { DataTypes, Model } from "sequelize";
-import { env } from "../../../config";
-import { PostgresqlDatabase } from "../database";
-import { ProductModel } from "./product";
+import { DataTypes, Model, Sequelize } from "sequelize";
 
-const database = new PostgresqlDatabase(env.POSTGRES_DB, env.POSTGRES_USER, env.POSTGRES_PASSWORD);
-const sequelize = database.getSequelize();
+export class StatusModel extends Model {
 
-export class StatusModel extends Model {};
-
-StatusModel.init({
-    id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        allowNull: false,
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false
-    },
-    updatedAt: {
-        type: DataTypes.DATE,
-        allowNull: false
+    static initModel(sequelize: Sequelize) {
+        StatusModel.init({
+            id: {
+                type: DataTypes.UUID,
+                primaryKey: true,
+                allowNull: false,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            createdAt: {
+                type: DataTypes.DATE,
+                allowNull: false
+            },
+            updatedAt: {
+                type: DataTypes.DATE,
+                allowNull: false
+            }
+        }, {
+            sequelize,
+            modelName: "status",
+            tableName: "statuses"
+        });
     }
-}, {
-    sequelize,
-    modelName: "category"
-});
-
-StatusModel.hasMany(ProductModel, { foreignKey: "statusId" });
+    
+};
