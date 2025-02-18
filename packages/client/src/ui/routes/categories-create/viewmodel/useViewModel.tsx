098f34e90deab31ed6a62c2b0fc2745749a70014
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-02-18 19:18:48 -0300

    add: implement category fetching in categories create view model

--- packages/client/src/ui/routes/categories-create/viewmodel/useViewModel.tsx
+++ packages/client/src/ui/routes/categories-create/viewmodel/useViewModel.tsx
@@ -1,3 +1,31 @@
+import { useEffect, useState } from "react";
+import { useRepositories } from "../../../../core";
+import { CategoryEntity } from "../../../../domain";
+
 export default function useViewModel() {
+
+    const { categoryRepository } = useRepositories();
+
+    /* --- States --- */
+    const [loading, setLoading] = useState<boolean>(true);
+    const [categories, setCategories] = useState<CategoryEntity[]>([]);
+
+    /* --- ----- --- */
+
+    useEffect(() => {
+        getAllCategories().then((categories) => {
+            setCategories(categories);
+            setLoading(false);
+        });
+    }, []);
+
+    const getAllCategories = async () => {
+        return await categoryRepository.findAll();
+    };
+
+    return {
+        loading,
+        categories
+    };
     
 };
