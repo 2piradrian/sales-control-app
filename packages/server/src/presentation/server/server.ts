5d5f242f7f80a0f5b12bb4ccd2b393c7533c183e
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-02-18 20:38:09 -0300

    add: integrate CORS middleware and update package dependencies

--- packages/server/src/presentation/server/server.ts
+++ packages/server/src/presentation/server/server.ts
@@ -1,4 +1,5 @@
 import express, { Router } from 'express';
+import cors from "cors";
 
 interface Options {
     port: number;
@@ -20,6 +21,13 @@ export class Server {
 
         /* Middlewares */
         this.app.use(express.json({ limit: '5mb' }));
+
+        /* Config */
+        this.app.use(cors({
+            origin: "http://localhost:5173",
+            methods: ["GET", "POST", "PUT", "DELETE"],
+            allowedHeaders: ["Content-Type", "Authorization"]
+        }));
 
         /* Routes */
         this.app.use(this.routes);
